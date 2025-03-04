952506fc51c4608e64a1e2f49fdb05e5243a1ea3
doankietdev <doankietdev@gmail.com>
2025-03-05 00:42:00 +0700

    setup mui

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,12 @@
+import Button from '@mui/material/Button';
+
 const Home = () => {
-  return <>Home</>
+  return  (
+    <>
+    <div>Hello world</div>
+    <Button variant="contained">Hello world</Button>
+    </>
+  )
 }
 
 export default Home
